30a52bb1dced53811fad8701de9111c2d94a38ec
Garvit Gupta <106311007+gsquare23@users.noreply.github.com>
2023-10-26 07:03:58 +0000

    ConversionsOfDatatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+// //let score = "33abc"
+// //let score = true
+//  let score = undefined
+// // let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// //"33" => 33
+// //"33abc" => Nan
+// // true => 1; false =>0
+
+
+// let isLoggedIn =1 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
+
+
+let number = 33
+let StringNumber =  String(number);
+console.log(StringNumber);
+console.log(typeof StringNumber);
