594e2470e79c9612b48e153255953c3914b0fb9e
Garvit Gupta <106311007+gsquare23@users.noreply.github.com>
2023-10-26 06:44:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use Strict"; //treat all js code as newer version
+
+alert(3+3) //we are using nodejs, not browser
+
+
+// number => 2 to power 53
+//bigint 
+//String => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol 
+
+//object 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
